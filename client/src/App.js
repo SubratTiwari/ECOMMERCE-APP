ca6f8fdc5e6051b898bae88ba0065038d782b568
SubratTiwari <tsubrat301@gmail.com>
2023-06-27 14:46:21 +0530

    Initial commit of all files to the repository

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,11 @@ import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/Users';
 import Orders from './pages/user/Orders';
 import Profile from './pages/user/Profile';
+import Products from './pages/Admin/Products';
+import UpdateProduct from './pages/Admin/UpdateProduct';
+import Search from './pages/Search';
+import ProductDetails from './pages/ProductDetails';
+import Categories from './pages/Categories';
 
 
 function App() {
@@ -26,6 +31,9 @@ function App() {
    <Routes>
          
          <Route path='/' element ={<HomePage/>}/>
+         <Route path='/product/:slug' element ={<ProductDetails/>}/>
+         <Route path='/search' element ={<Search/>}/>
+         <Route path='/categories' element ={<Categories/>}/>
          <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
           <Route path="user/orders" element={<Orders />} />
@@ -36,6 +44,8 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/product" element={<Products />} />
           <Route path="admin/create-users" element={<Users />} />
          
         </Route>
---
+++ client/src/pages/Categories.js
@@ -0,0 +1,14 @@
+import Layout from "./../components/Layout/Layout";
+import React , {useState,useEffect}from 'react'; 
+import useCategory from "../hooks/useCatgory";
+
+const Categories = () => {
+  return (
+    <Layout>
+        <h1>all ajdbja n</h1>
+      
+    </Layout>
+  )
+}
+
+export default Categories
