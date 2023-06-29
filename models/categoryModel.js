5d0b70023eb9731fd22aa4c39f98c6d9ea8f8745
SubratTiwari <tsubrat301@gmail.com>
2023-06-29 13:34:05 +0530

    Complete

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const categoryScgema = new mongoose.Schema({
-    name:{
-        type :String, 
-        required: true,
-        unique: true,
-    },
-    slug:{
-        type :String,
-        lowercase :true,
-    },
-}); 
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // required: true,
+    // unique: true,
+  },
+  slug: {
+    type: String,
+    lowercase: true,
+  },
+});
 
-export default mangoose.model("Category", categorySchema);
+export default mongoose.model("Category", categorySchema);
--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import colors from 'colors';
 import dotenv from 'dotenv'
 import morgan from "morgan"
 import connectDb from './config/db.js';
-import authRoutes from './routes/authRoute.js'
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoute.js';
+import productRoutes from "./routes/productRoutes.js";
+
 import cors from'cors'
 
 //configure env
@@ -24,6 +27,9 @@ app.use(morgan('dev'))
 
 //routes
 app.use('/api/v1/auth',authRoutes);
+
+app.use('/api/v1/category', categoryRoutes);
+app.use ('/api/v1/product', productRoutes);
 
 //rest api
 
